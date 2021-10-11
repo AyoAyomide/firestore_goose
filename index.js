19b4a31cd5a40e53772cd587622d4dccd4ac28a3
ayoayomide <ayoayomide124@gmail.com>
2021-10-11 09:51:49 +0100

    add added

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// const init_firebase = require('./src/init/_init.js');
 const createNewDocs = require('./src/set/create_docs.js');
 const addToDocs = require('./src/set/add_to_docs.js');
 class FireBox {
@@ -12,10 +11,4 @@ class FireBox {
         return new addToDocs(this.admin, path).save(key, value);
     }
 }
-// let path = { docPath: 'user/mydoc' };
-// let test = new FireBox(init_firebase());
-// // test.create({ path, key: "sam", value: "hello" });
-// test.add({ path, key: "sawfs", value: "brown" })
-//     .then(data => { console.log(data) })
-//     .catch(error => console.log(error));
 module.exports = FireBox;
