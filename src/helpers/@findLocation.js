134b39338f0fe717af9137edd5c9a6f0bbd43005
ayoayomide <ayoayomide124@gmail.com>
2021-10-13 15:44:55 +0100

    find location removed

--- src/helpers/@findLocation.js
+++
@@ -1,8 +0,0 @@
-async function findLocation(firestore, path, key) {
-    let query = firestore.doc(`${path}/location`);
-    let docs = await query.get();
-    if (!docs.exists) throw 'key not found in location Document';
-    return docs.data()[key];
-}
-
-module.exports = findLocation;
