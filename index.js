2553c5fbfbcb83eadae60d10ba5b4600a718319c
ayoayomide <ayoayomide124@gmail.com>
2022-03-06 09:32:23 +0100

    delete working fine

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const init_firebase = require('./src/init/_init.js');
 const random = require('./src/devstools/random');
-const bytesToSize = require('./src/devstools/byteToSize');
+// const bytesToSize = require('./src/devstools/byteToSize');
 // const looper = require('./src/devstools/loop');
 
 const addToDocs = require('./src/set/add_to_docs.js');
@@ -45,21 +45,21 @@ let value = random();
 //     test.add({ path, key, value })
 // });
 let data = {
-    "ySujRpRyfm6hE2oofX5e": ['5EogN', '5EogN', '5EogN', '5EogN', '5EogN'],
-    "ySujRpRyfm6hE2oofX5p": ['5EogN', '5EogN', '5EogN', '5EogN', '5EogN']
-}
-test.add({ path, key, value }, { force: true })
+        "ySujRpRyfm6hE2oofX5e": ['5EogN', '5EogN', '5EogN', '5EogN', '5EogN'],
+        "ySujRpRyfm6hE2oofX5p": ['5EogN', '5EogN', '5EogN', '5EogN', '5EogN']
+    }
+    // test.add({ path, key, value }, { force: true })
     // let sample = Buffer.byteLength(JSON.stringify(data));
     // console.log(bytesToSize(sample));
     // test.add({ path, key, value, maxHeight: 1 })
-    // test.getLast({ path, key: 'BoX3q' })
-    // test.getByID({ path, key: 'B4bsd' })
+    // test.getLast({ path, key: 'sam' })
+    // test.getByID({ path, key: 'sam' })
     // test.getAll({ path })
     // test.updateByID({ path, key: 'sam', childObject: 'wifes.jan5', value })
-    // test.updateByID({ path, key: 'NR9eA', childArrayAdd: 'wifes', value: "sa2k" })
-    // test.updateByID({ path, key: 'NR9eA', childArrayRemove: 'wifes', value: "sakpokl" })
-    // test.delete({ path, key: 'aq8ZN' })
-    // test.delete({ path, key: 'sam', childObject: 'wifes.jan2' })
+    // test.updateByID({ path, key: 'sam', childArrayAdd: 'wifes', value })
+    // test.updateByID({ path, key: 'sam', childArrayRemove: 'wifes', value: "sakpokl" })
+    // test.delete({ path, key: 'sam' })
+    // test.delete({ path, key: 'sam', childObject: 'wifes.jan5' })
     // .then(data => { console.log(data) })
     // .catch(data => { console.log(data) })
 
